cd0f14790bfc1f388d394422fbaee6b9ae1e1d48
Luke Gallagher <luke@hypergeometric.net>
2019-05-24 20:14:15 +1000

    Check files have the same number of topics
    Before `join` check the files have the same number of topics. Files are
    assumed to be sorted by qid, which is handled in pairwise-eval.sh.

--- pairwise-ttest.sh
+++ pairwise-ttest.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Perform pairwise t-test with Bonferroni correction
 
@@ -9,6 +9,24 @@ TMPB=$(mktemp -p .)
 HEADER="qid"
 COUNTER=0
 SYSMAP="System mapping:"$'\n'
+PREV=""
+
+err() {
+    echo "$1" 1>&2
+}
+
+check_topic_count() {
+    local prev=$1
+    local curr=$2
+    local prev_len=$(wc -l $prev | awk '{print $1}')
+    local curr_len=$(wc -l $curr | awk '{print $1}')
+    if [ $prev_len -ne $curr_len ]; then
+        err "error: different number of topics found"
+        err "$prev_len    $(basename $prev)"
+        err "$curr_len    $(basename $curr)"
+        exit 1
+    fi
+}
 
 for i in $@; do
     name="Sys$COUNTER"
@@ -17,11 +35,14 @@ for i in $@; do
     if [ $COUNTER -eq 0 ]; then
         cat $i > $TMPA
     elif [ $((COUNTER % 2)) -eq 0 ]; then
+        check_topic_count $PREV $i
         join $TMPB $i > $TMPA
     else
+        check_topic_count $PREV $i
         join $TMPA $i > $TMPB
     fi
     COUNTER=$((COUNTER + 1))
+    PREV=$(basename $i)
 done
 
 # flip the modulus, the counter was incremented on the last iteration
